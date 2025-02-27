c84a65d791389c3e4bc0523a7873798963e74292
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2025-02-27 02:14:05 +0100

    redo the entire error handling with thiserror

--- src/network/connection.rs
+++ src/network/connection.rs
@@ -1,68 +1,88 @@
+use etherparse::err::packet::SliceError;
 use etherparse::{SlicedPacket, TransportSlice, UdpHeader};
+use thiserror::Error;
 use tracing::{debug, info, instrument, trace, warn};
 
 use crate::network::{ConnectionPacket, PacketDirection};
 
 #[instrument(skip_all)]
-pub fn parse_connection_packet(port_filter: &[u16], bytes: Vec<u8>) -> Option<ConnectionPacket> {
+pub fn parse_connection_packet(
+    port_filter: &[u16],
+    bytes: Vec<u8>,
+) -> Result<ConnectionPacket, ConnectionPacketError> {
     let (udp, payload) = parse_udp(bytes)?;
     let direction = validate_ports(&port_filter, udp)?;
 
     if payload.len() <= 20 {
+        if payload.len() < 4 {
+            return Err(ConnectionPacketError::PayloadTooShort);
+        }
+
         let code = u32::from_be_bytes(payload[..4].try_into().unwrap());
         match code {
             0xFF => {
                 info!("handshake requested");
-                Some(ConnectionPacket::HandshakeRequested)
+                Ok(ConnectionPacket::HandshakeRequested)
             }
             404 => {
                 warn!("disconnected packet");
-                Some(ConnectionPacket::Disconnected)
+                Ok(ConnectionPacket::Disconnected)
             }
             _ => {
                 trace!("handshake established");
-                Some(ConnectionPacket::HandshakeEstablished)
+                Ok(ConnectionPacket::HandshakeEstablished)
             }
         }
     } else {
-        Some(ConnectionPacket::SegmentData(direction, payload))
+        Ok(ConnectionPacket::SegmentData(direction, payload))
     }
 }
 
 #[instrument(skip_all, fields(len = data.len()))]
-pub fn parse_udp(data: Vec<u8>) -> Option<(UdpHeader, Vec<u8>)> {
-    let packet = match SlicedPacket::from_ethernet(&data) {
-        Ok(p) => p,
-        Err(e) => {
-            debug!("failed: {e}");
-            return None;
-        }
-    };
+pub fn parse_udp(data: Vec<u8>) -> Result<(UdpHeader, Vec<u8>), ConnectionPacketError> {
+    let packet: SlicedPacket = SlicedPacket::from_ethernet(&data)?;
 
     // sanity checking the pcap filters
     let Some(transport) = packet.transport else {
         debug!("transport was not present");
-        return None;
+        return Err(ConnectionPacketError::TransportLayerNotPresent);
     };
 
     let TransportSlice::Udp(udp) = transport else {
         debug!("packet was not udp");
-        return None;
+        return Err(ConnectionPacketError::TransportLayerNotUdp);
     };
 
     trace!("complete");
 
-    Some((udp.to_header(), udp.payload().to_vec()))
+    Ok((udp.to_header(), udp.payload().to_vec()))
 }
 
-fn validate_ports(port_filter: &[u16], udp: UdpHeader) -> Option<PacketDirection> {
+fn validate_ports(
+    port_filter: &[u16],
+    udp: UdpHeader,
+) -> Result<PacketDirection, ConnectionPacketError> {
     let (src, dest) = (udp.source_port, udp.destination_port);
     if port_filter.contains(&src) {
-        Some(PacketDirection::Received)
+        Ok(PacketDirection::Received)
     } else if port_filter.contains(&dest) {
-        Some(PacketDirection::Sent)
+        Ok(PacketDirection::Sent)
     } else {
         trace!(src, dest, "incorrect ports");
-        return None;
+        return Err(ConnectionPacketError::IncorrectPorts);
     }
+}
+
+#[derive(Error, Debug)]
+pub enum ConnectionPacketError {
+    #[error("error while parsing network packet: {0}")]
+    EtherparsePacketError(#[from] SliceError),
+    #[error("transport layer is not present on packet")]
+    TransportLayerNotPresent,
+    #[error("transport layer is not udp protocol")]
+    TransportLayerNotUdp,
+    #[error("packet does not match the required ports")]
+    IncorrectPorts,
+    #[error("packet payload too short")]
+    PayloadTooShort,
 }
